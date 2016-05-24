34d7d36ee8e1fc4e87bc5b175182a722721954a4
terrychen0331 <yuh@dragon-dot.com>
2016-05-24 18:12:00 +0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,20 @@
+FOR %%b in (
+       "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+       "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+       "%ProgramFiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" 
+
+       "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+       "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+       "%ProgramFiles%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" 
+    ) do (
+    if exist %%b ( 
+       call %%b x86
+       goto build
+    )
+)
+  
+echo "Unable to detect suitable environment. Build may not succeed."
+
+:build
+
+msbuild SmartStoreNET.proj /p:DebugSymbols=false /p:DebugType=None /P:SlnName=SmartStoreNET /maxcpucount %*
